d8bfe4564718bec13387d22e7a4cb371572ff50f
Ting Plenting <tingplenting@gmail.com>
2015-12-15 22:23:27 +0700

    add error callback

--- RegexRouter.php
+++ RegexRouter.php
@@ -4,17 +4,28 @@ class RegexRouter {
     
     private $routes = array();
     
-    public function route($pattern, $callback) {
+    public function route($pattern, $callback)
+    {
         $this->routes[$pattern] = $callback;
     }
     
-    public function execute($uri) {
+    public function execute($uri, $err)
+    {
+
         foreach ($this->routes as $pattern => $callback) {
+
             if (preg_match($pattern, $uri, $params) === 1) {
                 array_shift($params);
                 return call_user_func_array($callback, array_values($params));
+            } 
+            else {
+                $error = $err;
             }
+
         }
+
+        call_user_func($error);
+
     }
     
 }
